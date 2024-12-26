e126950684c555a5771c0beac374586b0acdaaac
Timon <yurikikadet@gmail.com>
2024-12-26 17:02:24 +0600

    cd d

--- src/Vector3DArrayMain.java
+++ src/Vector3DArrayMain.java
@@ -3,8 +3,8 @@ public class Vector3DArrayMain {
         Vector3DArray arr = new Vector3DArray(5);
         Vector3D vector1 = new Vector3D(1, 2, 3);
         Vector3D vector2 = new Vector3D(1, 1, 1);
-        int[] intarr = {2,2,2,2,2};
-        Point3D point = new Point3D(2,2,2);
+        int[] intarr = {2, 2, 2, 2, 2};
+        Point3D point = new Point3D(2, 2, 2);
         System.out.println(arr.getLength());
         arr.setElement(3, vector1);
         arr.setElement(4, vector2);
